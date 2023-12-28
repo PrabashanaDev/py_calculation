6fd855403505b30ef548b78d814c19facbfd56cc
Chathura Prabashana <prabashana.dev@gmail.com>
2023-12-28 14:13:35 +0000

    Welcome message & Operation selection

---
+++ basiccal.py
@@ -0,0 +1,4 @@
+print('Hello Welcome to Basic Python Calculator') 
+name = input('What is your name?')
+print(f'Hello {name}! Welcome to Basic Python Calculator')
+print('Please select the operation you want to perform')
